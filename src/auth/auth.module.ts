85ea65892b8a901404c02e449f6416b7cede6310
Tai Le <taile.bpencil@gmail.com>
2024-02-14 14:22:20 +0700

    Feature: Protect endpoint with JWT strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AwsCognitoService } from './awsCognito.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AwsCognitoService]
+  providers: [AwsCognitoService, JwtStrategy],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })]
 })
 export class AuthModule {}
--- src/test/test.controller.ts
+++ src/test/test.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, UseGuards } from "@nestjs/common";
 import { TestService } from "./test.service";
 import { Test } from "./test.interface";
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('api/v1/tests')
 export class TestController {
     constructor(private testService: TestService) {}
 
+    @UseGuards(AuthGuard('jwt'))
     @Get()
     listAllTests(): Array<Test> {
         return this.testService.listAllTests();
